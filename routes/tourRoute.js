07d36150f3798b9cb2cf58c3b9bb640edaa1cddc
bilalshahid929 <bilalshahid929@gmail.com>
2021-11-11 01:11:07 +0500

    Added Param Middleware to tourRouter

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -2,6 +2,11 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
+router.param('id', (req, res, next, val) => {
+  console.log(`tour id is : ${val}`);
+  next();
+});
+
 router
   .route('/')
   .get(tourController.getAllTours)
