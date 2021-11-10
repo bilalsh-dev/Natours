aced5ae97eea6d799d6208cc33018a76b8b8aa2b
bilalshahid929 <bilalshahid929@gmail.com>
2021-11-11 04:09:44 +0500

    version 1

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -2,15 +2,12 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
-router.param('id', (req, res, next, val) => {
-  console.log(`tour id is : ${val}`);
-  next();
-});
+router.param('id', tourController.checkID);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
